ab0eca27495d91ab7de061b27f2122544edeb72f
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-12-17 02:42:05 +0100

    Make 5/2 faster

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -11,39 +11,75 @@ fn main() {
 
 #[derive(Debug)]
 struct Map {
-    ranges: Vec<Range>,
+    functions: Vec<Function>,
 }
 
 impl Map {
-    fn map(&self, number: usize) -> usize {
-        match self.ranges.iter().find_map(|r| r.map(number)) {
-            Some(mapped) => mapped,
-            None => number,
+    fn map(&self, number: isize) -> isize {
+        for range in &self.functions {
+            if !(range.src <= number && number < range.src + range.length) {
+                continue;
+            }
+            return range.dest + number - range.src;
         }
+        return number;
+    }
+    fn map_range(&self, range: Range) -> Vec<Range> {
+        let mut to_be_processed = vec![range];
+        let mut processed = Vec::new();
+        while to_be_processed.len() > 0 {
+            let range = to_be_processed.pop().unwrap();
+            let mut has_overlap = false;
+            for function in &self.functions {
+                let overlap_start = range.start.max(function.src);
+                let overlap_end = (range.start + range.length).min(function.src + function.length);
+                if overlap_end <= overlap_start {
+                    continue;
+                }
+                let overlap = Range {
+                    start: function.dest + overlap_start - function.src,
+                    length: overlap_end - overlap_start,
+                };
+                processed.push(overlap);
+                let before = Range {
+                    start: range.start,
+                    length: overlap_start - range.start,
+                };
+                if before.length > 0 {
+                    to_be_processed.push(before);
+                }
+                let after = Range {
+                    start: overlap_end,
+                    length: range.start + range.length - overlap_end,
+                };
+                if after.length > 0 {
+                    to_be_processed.push(after)
+                }
+                has_overlap = true;
+                break;
+            }
+            if !has_overlap {
+                processed.push(range);
+            }
+        }
+        processed
     }
 }
 
 #[derive(Debug)]
+struct Function {
+    src: isize,
+    dest: isize,
+    length: isize,
+}
+
+#[derive(Clone, Copy, Debug)]
 struct Range {
-    source_start: usize,
-    destination_start: usize,
-    length: usize,
+    start: isize,
+    length: isize,
 }
 
-impl Range {
-    fn can_map(&self, number: usize) -> bool {
-        self.source_start <= number && number < self.source_start + self.length
-    }
-    fn map(&self, number: usize) -> Option<usize> {
-        if !self.can_map(number) {
-            return None;
-        }
-        let offset = number - self.source_start;
-        Some(self.destination_start + offset)
-    }
-}
-
-fn parse_seeds(input: &str) -> Vec<usize> {
+fn parse_seeds(input: &str) -> Vec<isize> {
     input
         .lines()
         .next()
@@ -66,23 +102,23 @@ fn parse_maps(input: &str) -> Vec<Map> {
             continue;
         }
         if line == "" {
-            maps.push(Map { ranges });
+            maps.push(Map { functions: ranges });
             ranges = Vec::new();
             continue;
         }
         let mut iter = line.split(" ").map(|n| n.parse().unwrap());
-        ranges.push(Range {
-            destination_start: iter.next().unwrap(),
-            source_start: iter.next().unwrap(),
+        ranges.push(Function {
+            dest: iter.next().unwrap(),
+            src: iter.next().unwrap(),
             length: iter.next().unwrap(),
         });
     }
     maps
 }
 
-fn find_location<I>(seeds: I, maps: &Vec<Map>) -> usize
+fn find_location<I>(seeds: I, maps: &Vec<Map>) -> isize
 where
-    I: Iterator<Item = usize>,
+    I: Iterator<Item = isize>,
 {
     let mut lowest_location = None;
     for current_seed in seeds {
@@ -102,11 +138,11 @@ where
     lowest_location.unwrap()
 }
 
-fn part_1(seeds: &Vec<usize>, maps: &Vec<Map>) -> usize {
+fn part_1(seeds: &Vec<isize>, maps: &Vec<Map>) -> isize {
     find_location(seeds.to_owned().into_iter(), maps)
 }
 
-fn part_2(raw_seed_ranges: &Vec<usize>, maps: &Vec<Map>) -> usize {
+fn part_2(raw_seed_ranges: &Vec<isize>, maps: &Vec<Map>) -> isize {
     let mut seed_ranges = Vec::new();
     let mut seed_iter = raw_seed_ranges.iter();
     loop {
@@ -115,8 +151,19 @@ fn part_2(raw_seed_ranges: &Vec<usize>, maps: &Vec<Map>) -> usize {
             None => break,
         };
         let length = seed_iter.next().unwrap();
-        seed_ranges.push(*start..*start + length);
+        seed_ranges.push(Range {
+            start: *start,
+            length: *length,
+        });
     }
-    let seeds = seed_ranges.iter().flat_map(|it| it.clone());
-    find_location(seeds, maps)
+    let mut to_be_processed = seed_ranges;
+    let mut processed = Vec::new();
+    for map in maps {
+        while to_be_processed.len() > 0 {
+            processed.extend(map.map_range(to_be_processed.pop().unwrap()))
+        }
+        to_be_processed = processed.clone();
+        processed.clear();
+    }
+    to_be_processed.iter().map(|r| r.start).min().unwrap()
 }
