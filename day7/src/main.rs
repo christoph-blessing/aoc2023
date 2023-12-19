b14593bd86d32a324d64d378522f51cbb7057278
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-12-19 22:00:55 +0100

    Solve part 2 of day 7

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -2,7 +2,7 @@ use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::fs;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Hand {
     cards: Vec<char>,
     bid: usize,
@@ -22,18 +22,21 @@ enum HandType {
 const CARDS: [char; 13] = [
     'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2',
 ];
+const CARDS_JOKER: [char; 13] = [
+    'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J',
+];
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     let mut hands: Vec<Hand> = input.lines().map(parse).collect();
-    hands.sort_by(compare_hands);
+    hands.sort_by(create_comparer(&CARDS.to_vec(), determine_type));
     hands.reverse();
-    let answer1 = hands
-        .iter()
-        .enumerate()
-        .map(|(i, h)| (i + 1) * h.bid)
-        .sum::<usize>();
-    println!("Part 1 answer: {answer1}")
+    let answer1 = compute_winnings(&hands);
+    hands.sort_by(create_comparer(&CARDS_JOKER.to_vec(), determine_type_joker));
+    hands.reverse();
+    let answer2 = compute_winnings(&hands);
+    println!("Part 1 answer: {answer1}");
+    println!("Part 2 answer: {answer2}");
 }
 
 fn parse(line: &str) -> Hand {
@@ -43,16 +46,23 @@ fn parse(line: &str) -> Hand {
     Hand { cards, bid }
 }
 
-fn compare_hands(hand1: &Hand, hand2: &Hand) -> Ordering {
-    let type1 = determine_type(hand1);
-    let type2 = determine_type(hand2);
-    if type1 > type2 {
-        Ordering::Greater
-    } else if type1 < type2 {
-        Ordering::Less
-    } else {
-        break_tie(hand1, hand2)
-    }
+fn create_comparer<'a>(
+    ranks: &'a Vec<char>,
+    type_determiner: impl Fn(&Hand) -> HandType + 'a,
+) -> impl Fn(&Hand, &Hand) -> Ordering + '_ {
+    let compare = move |hand1: &Hand, hand2: &Hand| {
+        let type1 = type_determiner(hand1);
+        let type2 = type_determiner(hand2);
+        match type1.partial_cmp(&type2).unwrap() {
+            Ordering::Equal => break_tie(hand1, hand2, ranks),
+            order => order,
+        }
+    };
+    compare
+}
+
+fn determine_type_joker(hand: &Hand) -> HandType {
+    determine_type(&replace_jokers(hand))
 }
 
 fn determine_type(hand: &Hand) -> HandType {
@@ -77,10 +87,10 @@ fn determine_type(hand: &Hand) -> HandType {
     }
 }
 
-fn break_tie(hand1: &Hand, hand2: &Hand) -> Ordering {
+fn break_tie(hand1: &Hand, hand2: &Hand, ranks: &Vec<char>) -> Ordering {
     for (card1, card2) in hand1.cards.iter().zip(&hand2.cards) {
-        let position1 = CARDS.iter().position(|c| c == card1).unwrap();
-        let position2 = CARDS.iter().position(|c| c == card2).unwrap();
+        let position1 = ranks.iter().position(|c| c == card1).unwrap();
+        let position2 = ranks.iter().position(|c| c == card2).unwrap();
         if position1 < position2 {
             return Ordering::Less;
         } else if position1 > position2 {
@@ -88,4 +98,53 @@ fn break_tie(hand1: &Hand, hand2: &Hand) -> Ordering {
         }
     }
     panic!()
+}
+
+fn replace_jokers(hand: &Hand) -> Hand {
+    let mut counts: HashMap<&char, usize> = HashMap::new();
+    for card in &hand.cards {
+        match counts.get(card) {
+            Some(count) => counts.insert(card, count + 1),
+            None => counts.insert(card, 1),
+        };
+    }
+    if !counts.contains_key(&'J') {
+        return hand.clone();
+    }
+    let n_jokers = counts.get(&'J').unwrap();
+    if n_jokers == &hand.cards.len() {
+        return hand.clone();
+    }
+    let mut counts: Vec<(&char, usize)> = counts.into_iter().collect();
+    counts.sort_by_key(|(_, count)| *count);
+    counts.reverse();
+    let most_common = counts
+        .into_iter()
+        .map(|(c, _)| c)
+        .filter(|&c| c != &'J')
+        .next()
+        .unwrap();
+    let replaced: Vec<char> = hand
+        .cards
+        .iter()
+        .map(|c| {
+            if c == &'J' {
+                most_common.clone()
+            } else {
+                c.clone()
+            }
+        })
+        .collect();
+    Hand {
+        cards: replaced,
+        bid: hand.bid,
+    }
+}
+
+fn compute_winnings(hands: &Vec<Hand>) -> usize {
+    hands
+        .iter()
+        .enumerate()
+        .map(|(i, h)| (i + 1) * h.bid)
+        .sum::<usize>()
 }
