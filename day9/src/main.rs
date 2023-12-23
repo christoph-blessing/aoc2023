529e6e694e24b93b86b02d63d6edf68c33eba009
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-12-23 02:36:26 +0100

    Solve part 2 of day 9

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::iter;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,20 +13,38 @@ fn main() {
                 .collect()
         })
         .collect();
-    let extrapolated: Vec<isize> = histories.iter().map(extrapolate).collect();
-    let answer1: isize = extrapolated.into_iter().sum();
-    println!("Part 1 answer: {answer1}")
+    let answer1 = answer(&histories, next);
+    let answer2 = answer(&histories, previous);
+    println!("Part 1 answer: {answer1}");
+    println!("Part 2 answer: {answer2}")
 }
 
-fn extrapolate(history: &Vec<isize>) -> isize {
+fn answer(histories: &Vec<Vec<isize>>, direction: fn(&Vec<isize>, isize) -> isize) -> isize {
+    let extrapolated: Vec<isize> = histories
+        .iter()
+        .zip(iter::repeat(direction))
+        .map(|(history, f)| extrapolate(history, f))
+        .collect();
+    extrapolated.into_iter().sum()
+}
+
+fn extrapolate(history: &Vec<isize>, direction: fn(&Vec<isize>, isize) -> isize) -> isize {
     if history.iter().all(|&number| number == 0) {
         return 0;
     }
-    let differences: Vec<isize> = history
+    let differences = history
         .into_iter()
         .zip(history.into_iter().skip(1))
         .map(|(number1, number2)| number2 - number1)
         .collect();
-    let value = extrapolate(&differences);
-    history.into_iter().last().unwrap() + value
+    let value = extrapolate(&differences, direction);
+    direction(history, value)
+}
+
+fn next(history: &Vec<isize>, value: isize) -> isize {
+    *history.into_iter().last().unwrap() + value
+}
+
+fn previous(history: &Vec<isize>, value: isize) -> isize {
+    *history.into_iter().next().unwrap() - value
 }
